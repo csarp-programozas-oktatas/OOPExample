c6dbe8917ca2b6a7a01e54d9f8d7c096f9f7e48e
Csaba Gyuris <gycsaba@vasvari.hu>
2018-12-05 09:55:32 +0100

    Szamla osztály kifejlesztve

--- BankOsztaly/Program.cs
+++ BankOsztaly/Program.cs
@@ -6,6 +6,44 @@ using System.Threading.Tasks;
 
 namespace BankOsztaly
 {
+    class Szamla
+    {
+        private int egyenleg;
+        public Szamla()
+        {
+            this.egyenleg = 0;
+        }
+        public Szamla(int nyitoOsszeg)
+        {
+            if (nyitoOsszeg >= 0)
+                this.egyenleg = nyitoOsszeg;
+            else
+                throw new Exception("Számlát nem lehet nyitni negatív pénzösszeggel!");
+        }
+        public override string ToString()
+        {
+            return "Egyenleg: " + egyenleg;
+        }
+        public void befizetSzamlara(int osszeg)
+        {
+            if (osszeg > 0)
+            {
+                egyenleg += osszeg;
+            }
+            else
+                throw new Exception("Számlára nulla vagy negatív összeget nem lehet befizetni!");
+        }
+        public bool kiveszSzamlarol(int osszeg)
+        {
+            if (osszeg < egyenleg)
+            {
+                egyenleg -= osszeg;
+                return true;
+            }
+            else
+                throw new Exception("Csak akkora összeget lehet kivenni a számláról, amennyi a számlán van, többet nem!");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
