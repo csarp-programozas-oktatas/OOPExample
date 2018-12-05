9f653fac5e8944e237a0e477a06ddf21d1c8aef3
Csaba Gyuris <gycsaba@vasvari.hu>
2018-12-05 10:10:28 +0100

    Negyzet osztaly kifejlesztve

--- NegyzetOsztaly/Program.cs
+++ NegyzetOsztaly/Program.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace NegyzetOsztaly
 {
+    class Negyzet
+    {
+        private int oldal;
+
+        public Negyzet(int oldal)
+        {
+            if (oldal > 0)
+                this.oldal = oldal;
+            else
+                throw new Exception("Negatív oldalú négyzet nincs.");
+        }
+        public int getTerulet()
+        {
+            int terulet = oldal * oldal;
+            return terulet;
+        }
+        public int getKerulet()
+        {
+            int kerulet = 4 * oldal;
+            return kerulet;
+        }
+        public override string ToString()
+        {
+            return "Négyzet oldala: " + oldal;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
